a6fe451902d616a65bb9d92ee08898b28f7eee30
Nico De Witte <nico.dewitte@vives.be>
2016-06-10 17:00:21 +0200

    Add executable for gem

--- lib/ninetynine_snakes_server.rb
+++ lib/ninetynine_snakes_server.rb
@@ -1,5 +1,9 @@
 require "ninetynine_snakes_server/version"
 
 module NinetynineSnakesServer
-  puts "TODO Implement Server"
+  class Server
+    def initialize
+      puts "TODO Implement Server"
+    end
+  end
 end
